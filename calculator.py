b5f6f7da5343b5584d72f33f458ba107f853a718
Ezzeri Esa <ezzeri.esa@gmail.com>
2021-07-20 05:02:06 -0700

    up to 1.4

--- calculator.py
+++ calculator.py
@@ -9,6 +9,10 @@ class Literal(Expr):
         """ """
         self.value = value
 
+    def __repr__(self) -> None:
+        """ """
+        return f"Literal({str(self.value)})"
+
     def eval(self) -> int:
         """ """
         return self.value
@@ -19,6 +23,10 @@ class Plus(Expr):
         """ """
         self.left = left
         self.right = right
+
+    def __repr__(self) -> None:
+        """ """
+        return f"Plus({self.left}, {self.right})"
 
     def eval(self) -> Expr:
         """ """
@@ -31,12 +39,20 @@ class Times(Expr):
         self.left = left
         self.right = right
 
+    def __repr__(self) -> None:
+        """ """
+        return f"Times({self.left}, {self.right})"
+
     def eval(self) -> Expr:
         """ """
         return self.left.eval() * self.right.eval()
 
 
 class GetNumber(Expr):
+    def __repr__(self) -> None:
+        """ """
+        return "GetNumber()"
+
     def eval(self) -> int:
         """ """
         return int(input("enter a number: "))
@@ -47,10 +63,14 @@ class Print(Expr):
         """ """
         self.value = value
 
+    def __repr__(self) -> None:
+        """ """
+        return f"Print({self.value})"
+
     def eval(self) -> None:
         """ """
         print(self.value.eval())
 
 
 if __name__ == "__main__":
-    Print(Plus(Times(Literal(13), Literal(2)), Times(GetNumber(), Literal(7)))).eval()
+    print(Print(Plus(Times(Literal(13), Literal(2)), Times(GetNumber(), Literal(7)))))
